807a303aa4e7c87195d56cf3868d3ece291d32d1
Sampurnashrestha <shresthasampurna849@gmail.com>
2026-01-07 08:00:38 +0545

    added a contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,32 +1,48 @@
-import React from 'react'
+import React from "react";
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 const Contact = () => {
+  const contact = {
+    email: "sampurnashrsth@gmail.com",
+    number: 9843098663,
+    address: "Kathmandu,Nepal",
+    status: "Available for Internship, Freelance",
+    gitHub: "https://github.com/Sampurnashrestha",
+    linkedin: "www.linkedin.com/in/sampurna-shrestha-4238032aa",
+    text : "text-[16px] text-gray-500 cursor-default ",
+  };
   return (
     <>
-      <div className='min-h-[300px] min-w-full flex flex-col justify-center items-center '>
-      <h2>Contact</h2>
-      <span>sampurnashrsth@gmail.com</span>
+      <div className="min-h-[350px] min-w-full flex flex-col justify-center items-center  gap-1">
+        <h2 className="text-5xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent mb-2">Contact</h2>
+        <span className={contact.text}>{contact.email}</span>
 
-      <span>9843098663</span>
-      <span>Kathmandu,Nepal</span>
-      <span>Available for Internship, Freelance</span>
+        <span className={contact.text}>{contact.number}</span>
+        <span className={contact.text}>{contact.address}</span>
 
-      
-       <div className="flex text-[20px] lg:text-[28px]  justify-center lg:justify-start gap-3 mt-5  ">
-                  <a hre>
-                    <FaLinkedin className="text-gray-700 hover:text-[30px] hover:text-blue-500 transition" />
-                  </a>
-                  <a hre>
-                    <FaGithub className="text-gray-700 hover:text-[30px] hover:text-blue-500 transition" />
-                  </a>
-                  <a hre>
-                    <FaInstagram className="text-gray-700 hover:text-[30px] hover:text-blue-500 transition" />
-                  </a>
-                </div>
+        <div className="flex text-[20px] lg:text-[28px]  justify-center lg:justify-start gap-3 mt-5  mb-1 ">
+          <a href>
+            <FaLinkedin className="text-gray-700 hover:scale-105 hover:text-blue-500 transition" />
+          </a>
+          <a>
+            <FaGithub className="text-gray-700 hover:scale-105 hover:text-blue-500 transition" />
+          </a>
+          <a href>
+            <FaInstagram className="text-gray-700 hover:scale-105 hover:text-blue-500 transition" />
+          </a>
+        </div>
+        <span className={contact.text}>{contact.status}</span>
+        <a
+         href="/public/cv/resume.pdf"
+         download
+         className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent"
+        >
+          Download CV
+        </a>
       </div>
+    
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
